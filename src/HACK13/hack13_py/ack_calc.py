e22886106b1c56237eaa90ed03b3c4f0ba350fe8
hereticalchair <kjeromepohlman@gmail.com>
2025-10-25 11:05:52 -0400

    init ackermann calc

---
+++ src/HACK13/hack13_py/ack_calc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+
+class AckCalc(Node):
+  def __init__(self):
+    super().__init__("ack_calc")
+
+
+def main(args=None):
+  rclpy.init(args=None)
+
+  node = AckCalc()
+  rclpy.spin(node)
+  node.destroy_node()
+  rclpy.shutdown()
+
+
+if __name__ == "__main__":
+  main()
